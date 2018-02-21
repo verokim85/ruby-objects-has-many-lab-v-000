bf330a54d98e7f49e992699860d052391af13e3f
Veronica Kim <vero.a.kim@gmail.com>
2018-02-21 04:26:45 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,16 @@
+class Artist
+
+attr_accessor :name, :songs
+
+def initialize(name)
+  @name = name
+  @songs = []
+end
+
+# def add_song(song)
+#   song = Song.new(song)
+#   @song << song
+#   song.artist = self
+# end
+
+end
---
+++ lib/author.rb
@@ -0,0 +1,10 @@
+class Author
+
+attr_accessor :name, :post
+
+def initialize(name)
+  @name = name
+  @posts = []
+end
+
+end
